795334e71bf0e929d13412f806795e5d82023a2a
ishakhagi <ishakhagi10@gmail.com>
2025-09-08 20:09:04 +0200

    fix favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,19 +21,61 @@ export const metadata = {
     default: "Bailando - Mexikan Bar & Kitchen",
     template: "%s | Bailando - Mexikan Bar & Kitchen",
   },
-  description: "Bailando - Mexikan Bar & Kitchen",
+  description:
+    "Erleben Sie authentische mexikanische Küche im Bailando - Mexikan Bar & Kitchen. Genießen Sie köstliche Tacos, Burritos, erfrischende Cocktails und eine lebendige Atmosphäre. Ihr Ziel für mexikanisches Essen und Drinks!",
+  keywords: [
+    "mexikanisches Restaurant",
+    "Cocktail Bar",
+    "Tacos",
+    "Burritos",
+    "mexikanische Küche",
+    "Bailando",
+    "Bar",
+    "Kitchen",
+  ],
+  authors: [{ name: "Bailando Team" }],
+  creator: "Bailando - Mexikan Bar & Kitchen",
+  publisher: "Bailando - Mexikan Bar & Kitchen",
+  robots: "index, follow",
   openGraph: {
+    type: "website",
+    locale: "de_DE",
     title: "Bailando - Mexikan Bar & Kitchen",
-    description: "Bailando - Mexikan Bar & Kitchen",
+    description:
+      "Erleben Sie authentische mexikanische Küche im Bailando - Mexikan Bar & Kitchen. Genießen Sie köstliche Tacos, Burritos, erfrischende Cocktails und eine lebendige Atmosphäre.",
+    siteName: "Bailando - Mexikan Bar & Kitchen",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bailando - Mexikan Bar & Kitchen",
+    description:
+      "Erleben Sie authentische mexikanische Küche im Bailando - Mexikan Bar & Kitchen. Genießen Sie köstliche Tacos, Burritos, erfrischende Cocktails und eine lebendige Atmosphäre.",
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.png", type: "image/png" },
+      { url: "/favicon.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon.png", sizes: "180x180", type: "image/png" }],
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/favicon.png",
+        color: "#000000",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
       <head>
-        <meta name="author" content="Themeservices" />
-        <link rel="icon" href="/favicon.png" sizes="any" />
+        <meta name="author" content="Bailando Team" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="msapplication-TileColor" content="#000000" />
       </head>
       <body className={`${epilogue.variable} ${roboto.variable}`}>
         {children}
